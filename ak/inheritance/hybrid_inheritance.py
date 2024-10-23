fcee3c3a6930aff5792f9971b98a22ab5616671c
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-23 16:38:50 +0530

    ..

---
+++ ak/inheritance/hybrid_inheritance.py
@@ -0,0 +1,17 @@
+# syntax
+# class baseclass:
+#     def funbase(self):
+#         print('class base')
+#
+# class a(baseclass):
+#     def funa(self):
+#         print('class a')
+#
+# class b(baseclass):
+#     def funb(self):
+#         print('class b')
+#
+# class c (a,b):
+#     def func(self):
+#         print('class c')
+
