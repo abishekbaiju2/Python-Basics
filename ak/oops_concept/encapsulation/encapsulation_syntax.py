9a591e52aafd05ead9102a194c121e88319ddf0d
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-24 16:24:55 +0530

    ..

--- ak/oops_concept/encapsulation/encapsulation_syntax.py
+++ ak/oops_concept/encapsulation/encapsulation_syntax.py
@@ -8,21 +8,42 @@
 
 # public access modifier
 
-class person:
-    def __init__(self,name,email):
-        self.name=name
-        self.email=email
-
-    def display(self):
-        print(self.name)
-        print(self.email)
-
-obj1=person('arun','arun@gmail.com')
-obj1.display()
-print(obj1.name)
-obj1.name='akhil'
-obj1.email='akhil@gmail.com'
-obj1.display()
+# class person:
+#     def __init__(self,name,email):
+#         self.name=name
+#         self.email=email
+#
+#     def display(self):
+#         print(self.name)
+#         print(self.email)
+#
+# obj1=person('arun','arun@gmail.com')
+# obj1.display()
+# print(obj1.name)
+# obj1.name='akhil'
+# obj1.email='akhil@gmail.com'
+# obj1.display()
 
 # private access modifier
 
+class person:
+    def __init__(self,name,email):
+        self.__name=name
+        self.__email=email
+
+    def __display(self): # private
+        print(self.__name)
+        print(self.__email)
+
+obj2=person('arun','arun@gmail.com')
+print(obj2._person__name)
+print(obj2._person__email)
+obj2._person__name='akhil'
+obj2._person__email='akhil@gmail.com'
+obj2._person__display()
+
+
+
+# name mangling
+# it is a method of giving access to the private data members  and the methods inside a class
+# _classname__datamember
