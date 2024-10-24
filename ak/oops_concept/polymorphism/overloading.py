8f5cfc4637dec1c15c43a1089ca7fff89607d16f
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-24 17:21:25 +0530

    ..

--- ak/oops_concept/polymorphism/overloading.py
+++ ak/oops_concept/polymorphism/overloading.py
@@ -1,10 +1,35 @@
+from gettext import install
+
+
+# class maths:
+#     def add(self,a,b):
+#         print(a+b)
+#     def add(self,a,b,c):
+#         print(a+b+c)
+# x=maths()
+# x.add(1,2,3)
+# print(x)
+# x.add(2,3)
+# print(x)
+
+# you achieve method overloading directly using multipledispatch we can achieve method  overloading
+# multipledispatch : external package
+# pip install multipledispatch
+
+from multipledispatch import dispatch
+
 class maths:
+    @dispatch(int,int)
     def add(self,a,b):
         print(a+b)
+    @dispatch(int,int,int)
     def add(self,a,b,c):
         print(a+b+c)
+
 x=maths()
-x.add(1,2,3)
-print(x)
 x.add(2,3)
-print(x)
+x.add(1,2,3)
+
+
+# what is a decorator ?
+# extend a function without any modification
