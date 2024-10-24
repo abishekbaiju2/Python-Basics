d95d937d45c88514f1b64438d94de6262de5f015
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-24 15:58:48 +0530

    ..

---
+++ ak/oops_concept/encapsulation/encapsulation_syntax.py
@@ -0,0 +1,7 @@
+# syntax
+
+class encapsulation:
+    def __init__(self,name,address,phone):
+        self.name=name # public data member
+        self._address=address # protected data members
+        self.__phone= phone # private data member
