e66697549fb6fff48688a5620081afdc81e9ce24
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-28 16:03:12 +0530

    .

--- oops_concept/polymorphism/overriding.py
+++ oops_concept/polymorphism/overriding.py
@@ -2,6 +2,42 @@
 # inheritance always required
 # replacing a function body with same name and args
 
-class A:
-    def functionA(self):
-        print('welcome to python')
+# class A:
+#     def functionA(self):
+#         print('welcome to python')
+#
+# class B(A): # here B inherits A
+#     def functionA(self):
+#         print('welcome to javascript')
+#
+# obj=B()
+# obj.functionA() # here B functionA overrides A functionA (replace)
+
+
+# create a parent class shapes with a function area() that returns zero
+# create a child class rectangle that passes two values length and width with function area
+# create a child class circle that passes radius with a function area
+
+class shapes:
+    def area(self):
+        return 0
+
+class rectangle(shapes):
+    def  __init__(self,length ,width):
+        self.length=length
+        self.width=width
+
+    def area(self):
+        return self.length*self.width
+
+obj=rectangle(2,5)
+print(obj.area())
+
+class circle(shapes):
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return 3.14*self.radius
+
+obj1=circle(5)
+print(obj1.area())
