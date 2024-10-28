a9f2ef25a204d82cabea1be6b9f28f3e6848e45d
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-10-28 15:47:41 +0530

    .

---
+++ oops_concept/polymorphism/overriding.py
@@ -0,0 +1,7 @@
+# same function name with same number of arguments
+# inheritance always required
+# replacing a function body with same name and args
+
+class A:
+    def functionA(self):
+        print('welcome to python')
