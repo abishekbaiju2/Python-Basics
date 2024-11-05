254a96685925a784288471a240dcda7034fdeb3c
abishekbaiju2 <abishekbaiju2@gmail.com>
2024-11-05 17:07:30 +0530

    .

--- python_to_mysql/database_creation.py
+++ python_to_mysql/database_creation.py
@@ -1,1 +1,15 @@
 import mysql.connector
+
+dbconnect=mysql.connector.connect(host='localhost',user='root',password='password@123')
+# create cursor object
+#  it is used to execute sql commands and also to fetch datas in mysql
+
+mycursor=dbconnect.cursor()
+
+sql='create database products'
+
+mycursor.execute(sql)
+
+dbconnect.commit()
+
+print('database created successfully...')
